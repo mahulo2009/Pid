b91d24b0f9647d85ee892d6fa7163a6577ce8f2d
mhuertas <manuel.huertas.lopez@gmail.com>
2018-07-15 19:33:05 +0100

    First version Pid controller

--- src/Pid.cpp
+++ src/Pid.cpp
@@ -1,7 +1,7 @@
 #include "Pid.h"
 
 
-Pid::Pid() 
+Pid::Pid() : error_sum_(0), error_last_(0)
 {
 }
 
@@ -9,15 +9,51 @@ void Pid::setTarget(float target)
 {
 	this->target_=target;
 }
+
 void Pid::setKp(float Kp)
 {
 	this->Kp_=Kp;
 }
-float Pid::update(float measure)
+
+void Pid::setKi(float Ki)
+{
+	this->Ki_=Ki;
+}
+
+void Pid::setKd(float Kd)
+{
+	this->Kd_=Kd;
+}
+
+void Pid::setMaxWindup(float max_windup)
+{
+	this->max_windup_=max_windup;
+}
+
+void Pid::setAlpha(float alpha)
+{
+	this->alpha_=alpha;	
+}
+
+
+float Pid::update(float measure,float dt)
 {
 	float error = this->target_ - measure;
+	float error_delta = error - error_last_;
+	this->error_sum_ += error * dt;
+	
+	if (this->error_sum_>this->max_windup_) 
+		this->error_sum_=this->max_windup_;
+	else if (this->error_sum_<-this->max_windup_) 
+		this->error_sum_=-this->max_windup_;
+
+	this->error_last_ = error;
+
 	float p = this->Kp_ * error;
-	float u = p;
+	float i = this->Ki_ * error_sum_;
+	float d = this->Kd_ * ( this->alpha_ * error_delta/dt + (1-this->alpha_) * error);
+
+	float u = p + i + d;
 	#ifdef PID_DEBUG
   	Serial.print("Pid::update:");
   	Serial.print("\t");
@@ -31,4 +67,16 @@ float Pid::update(float measure)
   	Serial.print("\n");
   	#endif
 	return u;
+}
+
+void Pid::reset()
+{
+	this->Kp_=0; 	
+  	this->Ki_=0; 	
+  	this->Kd_=0;
+  	this->max_windup_=0;
+  	this->alpha_=0;	
+  	this->target_=0;
+  	this->error_sum_=0;	
+  	this->error_last_=0;
 }
--- src/Pid.h
+++ src/Pid.h
@@ -3,18 +3,27 @@
 
 #include "Arduino.h"
 
-#define PID_DEBUG 1
+//#define PID_DEBUG 1
 
 class Pid {
   public:
 	Pid();
 	void setTarget(float target);
 	void setKp(float Kp);
-	float update(float measure);
+	void setKi(float Ki);
+	void setKd(float Kd);
+	void setMaxWindup(float max_windup);
+	void setAlpha(float alpha);
+	float update(float measure,float dt);
+	void reset();
   private:
   	float Kp_; 	
+  	float Ki_;
+  	float Kd_;
+  	float max_windup_;
+  	float alpha_;
   	float target_;
-  	long start_times_;
-  	long last_timestamp_;
+  	float error_sum_;
+  	float error_last_;
 };
 #endif
